38e46c1c7b8f29224a5103e904cbc6317f0f5816
samuel de santis <samuel@samuels-Mac-Pro.local>
2014-08-26 12:29:55 +0200

    Initial Commit

---
+++ OGLUIKit/OGLUIKit.h
@@ -0,0 +1,13 @@
+//
+//  OGLUIKit.h
+//  OGLUIKit
+//
+//  Created by samuel de santis on 26/08/2014.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OGLUIKit : NSObject
+
+@end
---
+++ OGLUIKit/OGLUIKit.m
@@ -0,0 +1,13 @@
+//
+//  OGLUIKit.m
+//  OGLUIKit
+//
+//  Created by samuel de santis on 26/08/2014.
+//
+//
+
+#import "OGLUIKit.h"
+
+@implementation OGLUIKit
+
+@end
